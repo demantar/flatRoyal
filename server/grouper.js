34b3dbf40ad16f1430787dea5cee5731366f2628
demantar <BeVM01@rvkskolar.is>
2018-08-27 18:27:33 +0000

    added player cont

--- server/grouper.js
+++ server/grouper.js
@@ -4,6 +4,7 @@
   module.exports = async (socket, io) => {
     socket.join(roomname + "");
     roompop ++;
+    io.to(roomname + "").emit("message", (2 - roompop) + " player/s left");
     if (roompop == 2) {
       console.log("group formed");
       io.to(roomname + "").emit("message", "Group found");
