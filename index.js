bfd518aaa91972c6d8914222ff4bdfe007433a79
demantar <BeVM01@rvkskolar.is>
2018-08-27 16:19:35 +0000

    added game page

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io').listen(http);
 
-const path = require("path");
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, "client")));
-app.listen(3000, () => {
+http.listen(3000, () => {
   console.log("listening on port :3000");
+});
+
+io.on('connetion', (socket) => {
+  console.log("A user connected.");
+  socket.on('dissconect', () => {
+    console.log("A user dissconntected");
+  });
 });
