f4e8b1dd898f8abbacce8ae5a433cef9718f1fec
demantar <BeVM01@rvkskolar.is>
2018-08-28 17:26:24 +0000

    set ip to be local

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const path = require('path');
 const grouper = require('./server/grouper');
 
 app.use(express.static(path.join(__dirname, "client")));
-http.listen(3000, () => {
-  console.log("listening on port :3000");
+http.listen(3000, "192.168.1.168", () => {
+  console.log("listening on: 192.168.1.168:3000");
 });
 
+let totalUsers = 0;
+
 io.on('connection', (socket) => {
-  console.log("A user connected.");
+  totalUsers ++;
+  console.log("A user connected. Now there are " + totalUsers + " sockets");
   grouper(socket, io);
   socket.on('dissconect', () => {
-    console.log("A user dissconntected");
+    totalUsers--;
+    console.log("A user connected. Now there are " + totalUsers + " sockets");
   });
 });
